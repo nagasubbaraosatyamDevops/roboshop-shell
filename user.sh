7945ca95e14b534b45ffbd3c9cb3993b37960cce
nagas <nagsubbaraosatyam@gmail.com>
2024-02-20 22:54:36 +0530

    creating some more bash files

--- user.sh
+++ user.sh
@@ -6,10 +6,12 @@ dnf install nodejs -y
 useradd roboshop
 mkdir /app
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
-cd /app
 unzip /tmp/user.zip
 cd /app
 npm install
+
+dnf install mongodb-org-shell -y
+mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
 systemctl daemon-reload
 systemctl enable user
-systemctl start user
+systemctl restart user
