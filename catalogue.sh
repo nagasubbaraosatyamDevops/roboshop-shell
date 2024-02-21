13e0b2764f98abc359d1b66b3f89b99522b291f0
nagas <nagsubbaraosatyam@gmail.com>
2024-02-21 16:51:48 +0530

    updating again catalogue.sh

--- catalogue.sh
+++ catalogue.sh
@@ -2,6 +2,7 @@ cp catalogue.service /etc/systemd/system/catalogue.service
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
+dnf install nodejs -y
 useradd roboshop
 mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
@@ -13,4 +14,4 @@ dnf install mongodb-org-shell -y
 mongo --host mongodb.sdevopsb74.online </app/schema/catalogue.js
 systemctl daemon-reload
 systemctl enable catalogue
-systemctl start catalogue
+systemctl restart catalogue
